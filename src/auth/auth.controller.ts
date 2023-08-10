dc277fdf16e949192f1fd9a0762832cb4d66c306
bryan athallah <bryanathallah7@gmail.com>
2023-08-10 22:46:03 +0700

    belajar day 1

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller('home')
+@Controller()
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Get('hunter')
   sayHi(): string {
     return this.authService.sayHi('bryan');
+  }
+
+  @Post('home')
+  getNumber(@Body() data): number {
+    const { angka } = data;
+    console.log(angka);
+    return this.authService.giveNumber(angka);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
-interface User {
-  name: string;
-  age: number;
-  job: string;
-}
 @Injectable()
 export class AuthService {
   private myName = 'bryan';
 
   sayHi(value: string): string {
     return `haloo nama saya adalah ${value}`;
+  }
+  giveNumber(value: number): number {
+    return value * 2;
   }
 }
